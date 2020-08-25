40a05a8e6d906b8562f2c2ac897d1887e717c8cd
Raghuvara Padma <raghuvarapadma@gmail.com>
2020-08-25 17:16:29 -0400

    major update - added comments, improved user experience, updated try except catches

--- main_tic_tac_toe.py
+++ main_tic_tac_toe.py
@@ -1,72 +1,97 @@
 # main_tic_tac_toe.py
 
+# import all methods from methods.py
 from methods import *
 
+
 def main():
+    """
+    Function which executes the entire program
+    :return: no return value
+    """
+
     print("Welcome to Tic Tac Toe!")
-    board = ["#"] * 9
+
+    # creates and displays board to the user
+    board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
     display_board(board)
 
+    # while True, both users are playing the game, and once the game ends, while loop breaks
     while True:
-        player = random.randint(1,2)
-        if (player == 1):
+
+        # chooses if player 1 or player 2 gets to select the marker
+        player = random.randint(1, 2)
+
+        # this block of code sets X and O equal to player 1 and 2 depending on their preferences
+        player_one = ""
+        player_two = ""
+        if player == 1:
             print("Player 1 gets to choose whether they are X or O!")
             player_one = player_input()
-            if (player_one == "X"):
+            if player_one == "X":
                 player_two = "O"
                 print("Player 1 is X and Player 2 is O")
-            elif (player_one == "O"):
+            elif player_one == "O":
                 player_two = "X"
                 print("Player 1 is O and Player 2 is X")
-        elif (player == 2):
+        elif player == 2:
             print("Player 2 gets to choose whether they are X or O!")
             player_two = player_input()
-            if (player_two == "X"):
+            if player_two == "X":
                 player_one = "O"
                 print("Player 2 is X and Player 1 is O")
-            elif (player_two == "O"):
+            elif player_two == "O":
                 player_one = "X"
-                print("Player 2 is O and Player 1 is X") 
-                
-        first_Player = choose_first()
-        if (first_Player == "Player 1"):
-            cP = 1
+                print("Player 2 is O and Player 1 is X")
+
+        # this block of code decides on which player gets to first (cp tracks the turns)
+        first_player = choose_first()
+        if first_player == "Player 1":
+            cp = 1
             print("Player 1 gets to go first")
         else:
-            cP = 2
+            cp = 2
             print("Player 2 gets to go first")
-            
+
+        # this block of code starts the game
         game = True
         while game:
-            pos = player_choice(board)
-            if (cP % 2 == 1):
+
+            # depending on which player goes first, they choose a position, place their marker, and there is a check
+            # to see if they have won
+            pos = player_choice(board, cp)
+            if cp % 2 == 1:
                 place_marker(board, player_one, pos)
                 display_board(board)
-                winC1 = win_check(board, player_one)
-                if (winC1 == True):
+                win_c1 = win_check(board, player_one)
+                if win_c1:
                     print("Player One has won!")
                     break
-            elif (cP % 2 == 0):
+            elif cp % 2 == 0:
                 place_marker(board, player_two, pos)
                 display_board(board)
-                winC2 = win_check(board, player_two)
-                if (winC2 == True):
+                win_c2 = win_check(board, player_two)
+                if win_c2:
                     print("Player Two has won!")
                     break
-            if (board_check(board) == True):
+
+            # if the board has filled up, the game is a tie, or else, the next player goes
+            if board_check(board):
                 print("The game is a tie!")
                 break
             else:
-                cP = cP + 1
+                cp = cp + 1
                 continue
-                
+
+        # asks user if they want to replay (breaks loop if "no")
         replay = replay_func()
-        if (replay == True):
-            board = ["#"] * 9
+        if replay:
+            board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
             print("The game has restarted!")
         else:
             print("Thanks for playing!")
             break
+
 
 if __name__ == "__main__":
     main()
--- methods.py
+++ methods.py
@@ -1,109 +1,188 @@
 # methods.py
 
 import random
+import re
+
 
 def display_board(board):
+    """
+    Displays the board to the user whenever called
+    :param board: passes in the board (in the form of an array)
+    :return: no return value
+    """
+
+    # prints out board to the user
     i = 0
     while i < len(board):
-        print ("| " + board[i] + " | " + board[i+1] + " | " + board[i+2] + "|")
+        print("| " + board[i] + " | " + board[i + 1] + " | " + board[i + 2] + "|")
         i = i + 3
 
-def player_input():
-    pInput = ""
-    while (not(pInput == "O" or pInput == "X")):
-        pInput = input("Please select X or O: ")
-    return pInput
 
-def choose_position():
+def player_input():
+    """
+    Takes in input from user to track what marker they want
+    :return:
+    """
+
+    # takes in marker input
+    user_marker_choice = ""
+    while not (user_marker_choice == "O" or user_marker_choice == "X"):
+        user_marker_choice = input("Please select X or O: ")
+    return user_marker_choice
+
+
+def choose_position(cp):
+    """
+    Asks the user what position they want to place their marker at
+    :return: returns user's choice of position
+    """
+
+    # goes through while loop to ensure user does not enter in invalid input
+    player = ""
     while True:
         try:
-            pValue = int(input("Please select a position: "))
-        except TypeError:
+            if cp % 2 == 0:
+                player = "2"
+            elif cp % 2 == 1:
+                player = "1"
+            user_position_choice = int(input("Player " + player + " please select a position: "))
+        except ValueError:
             print("Please enter a valid input!")
             continue
         else:
-            print("You entered " + str(pValue) + ".")
-            return pValue
-            break
+            print("You entered " + str(user_position_choice) + ".")
+            return user_position_choice
+
 
 def place_marker(board, marker, position):
-    board[position-1] = marker
+    """
+    Since the board is a array, just assigns the marker to a specific position in the array
+    :param board: passes in the board (in the form of an array)
+    :param marker: the marker itself (X or O)
+    :param position: position marker should be placed at
+    :return: no return value
+    """
+
+    # when the user enters a position, they are counting one more than the actual position (arrays start at 0)
+    board[position - 1] = marker
+
 
 def win_check(board, marker):
-    if ((marker == board[0]) and (marker == board[1]) and (marker == board[2])):
+    """
+    Checks if there is 3 in a row on the board
+    :param board: passes in the board (in the form of an array)
+    :param marker: the marker itself (X or O)
+    :return: returns True or False depending on if the board contains a match
+    """
+
+    if (marker == board[0]) and (marker == board[1]) and (marker == board[2]):
         return True
-    elif ((marker == board[3]) and (marker == board[4]) and (marker == board[5])):
+    elif (marker == board[3]) and (marker == board[4]) and (marker == board[5]):
         return True
-    elif ((marker == board[6]) and (marker == board[7]) and (marker == board[8])):
+    elif (marker == board[6]) and (marker == board[7]) and (marker == board[8]):
         return True
-    elif ((marker == board[0]) and (marker == board [3]) and (marker == board[6])):
+    elif (marker == board[0]) and (marker == board[3]) and (marker == board[6]):
         return True
-    elif ((marker == board[1]) and (marker == board [4]) and (marker == board[7])):
+    elif (marker == board[1]) and (marker == board[4]) and (marker == board[7]):
         return True
-    elif ((marker == board[2]) and (marker == board [5]) and (marker == board[8])):
+    elif (marker == board[2]) and (marker == board[5]) and (marker == board[8]):
         return True
-    elif ((marker == board[0]) and (marker == board [4]) and (marker == board[8])):
+    elif (marker == board[0]) and (marker == board[4]) and (marker == board[8]):
         return True
-    elif ((marker == board[2]) and (marker == board [4]) and (marker == board[6])):
+    elif (marker == board[2]) and (marker == board[4]) and (marker == board[6]):
         return True
     else:
         return False
 
+
 def choose_first():
-    chooseFirstVar = random.randint(1,2)
-    if (chooseFirstVar == 1):
+    """
+    Chooses which players gets to go first
+    :return: returns player who goes first
+    """
+
+    choose_first_player = random.randint(1, 2)
+    if choose_first_player == 1:
         return "Player 1"
     else:
         return "Player 2"
 
 
 def space_check(board, space):
-    if (board[space-1] == "#"):
+    """
+    Checks if space that the user choice is already occupied
+    :param board: passes in the board (in the form of an array)
+    :param space: specific position that needs to be checked (x-1 since board in format of an array)
+    :return: either returns True or False depending on if space is available
+    """
+
+    if re.search("\d", board[space - 1]):
         return True
     else:
         print("Can't place your marker here!")
         return False
 
-def board_check(board):
-    i = 0
-    sum_board = 0
-    retVal = True
-    while i < len(board):
-        if ((board[i] == "X") or (board[i] == "O")):
-            sum_board += 1
-        else:
-            pass
-        i = i + 1
-    if (i == sum_board):
-        return True
-    else:
-        return False
 
-def player_choice(board):
+def board_check(board):
+    """
+    Checks if the game is currently a tie (sees if board is filled)
+    :param board: passes in the board (in the form of an array)
+    :return: either returns True or False depending on if the board is filled
+    """
+
+    i = 0
+    while i < len(board):
+        if (board[i] == "X") or (board[i] == "O"):
+            i = i + 1
+            continue
+        else:
+            return False
+
+    return True
+
+
+def player_choice(board, cp):
+    """
+    Asks the player where they would like to place their marker on the board
+    :param cp: variable which keeps track of player's turn
+    :param board: asses in the board (in the form of an array)
+    :return: returns the position they would like to place their marker
+    """
+
     position = 0
-    retVal = False
-    while (retVal == False):
-        position = choose_position()
-        retVal = space_check(board, position)
-        if (retVal == True):
+    pos_available = False
+    while not pos_available:
+        while True:
+            position = choose_position(cp)
+            if position not in range(1, 10):
+                print("Please enter in a position from 1 through 9!")
+                continue
+            else:
+                break
+        pos_available = space_check(board, position)
+
+        if pos_available:
             break
         else:
             continue
     return position
 
+
 def replay_func():
+    """
+    Determines whether players want to play again
+    :return: either returns True or False depending on if users want to play again
+    """
     while True:
         try:
             response = input("Do you want to play again? ")
-        except TypeError:
+        except ValueError:
             print("Please enter a valid response!")
         else:
-            if (response.lower() == "yes"):
+            if response.lower() == "yes":
                 return True
-                break
-            elif (response.lower() == "no"):
+            elif response.lower() == "no":
                 return False
-                break
             else:
                 print("Please enter a valid response!")
                 continue
